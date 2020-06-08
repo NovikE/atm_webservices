80d23bd7da0c1d01a629ef9024b97c0512be4838
Alena Novik <alena_novik1@epam.com>
2020-06-08 13:56:39 +0300

    webservice with RestAssured

---
+++ src/test/java/model/Company.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Company {
+
+    private String name;
+    private String catchPharse;
+    private String bs;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCatchPharse() {
+        return catchPharse;
+    }
+
+    public void setCatchPharse(String catchPharse) {
+        this.catchPharse = catchPharse;
+    }
+
+    public String getBs() {
+        return bs;
+    }
+
+    public void setBs(String bs) {
+        this.bs = bs;
+    }
+}
---
+++ src/test/java/tests/RestAssuredTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import model.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+
+    @BeforeTest
+    public void initTest() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void checkStatusCode() {
+        Response response = RestAssured.when()
+                 .get("/users")
+                 .andReturn();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        }
+
+    @Test
+    public void checkResponseHeader(){
+         Response response = RestAssured.when()
+                 .get("/users")
+                 .andReturn();
+         String rpContentTypeHeader = response.getHeader("Content-Type");
+         Assert.assertEquals(rpContentTypeHeader, "application/json; charset=utf-8");
+        }
+    @Test
+    public void checkResponseBody(){
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+        ResponseBody responseBody = response.getBody();
+        User[] users = responseBody.as(User[].class);
+        Assert.assertEquals(users.length, 10);
+    }
+}
